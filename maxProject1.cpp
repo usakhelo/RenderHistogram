9f72be85cb237ccbf3fe41fe17d176ec2d5b30a9
usakhelo <usakhelo@users.noreply.github.com>
2016-11-21 13:13:21 -0500

    interface added, rendering added

--- maxProject1.cpp
+++ maxProject1.cpp
@@ -13,6 +13,7 @@
 //***************************************************************************/
 
 #include "maxProject1.h"
+#include "WindowsMessageFilter.h"
 
 #define renderHistogram_CLASS_ID	Class_ID(0x7354e284, 0x6556a2a9)
 
@@ -20,34 +21,42 @@
 class renderHistogram : public UtilityObj 
 {
 public:
-		
+
 	//Constructor/Destructor
 	renderHistogram();
 	virtual ~renderHistogram();
 
 	void DeleteThis() { }
-	
+
 	void BeginEditParams(Interface *ip,IUtil *iu);
 	void EndEditParams(Interface *ip,IUtil *iu);
 
 	void Init(HWND hWnd);
 	void Destroy(HWND hWnd);
-	
+
 	// Singleton access
 	static renderHistogram* GetInstance() { 
 		static renderHistogram therenderHistogram;
 		return &therenderHistogram; 
 	}
 
+	bool CheckWindowsMessages(HWND hWnd);
+	void RenderFrames();
+
 private:
 
 	static INT_PTR CALLBACK DlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+	ISpinnerControl*  fromFrame;
+	ISpinnerControl*  toFrame;
+	ISpinnerControl*  ratioVal;
+
 	HWND   hPanel;
 	IUtil* iu;
+	Interface *ip;
 };
 
-
+class maxRndProgressCB;
 class renderHistogramClassDesc : public ClassDesc2 
 {
 public:
@@ -60,7 +69,7 @@ public:
 
 	const TCHAR* InternalName() 			{ return _T("renderHistogram"); }	// returns fixed parsable name (scripter-visible name)
 	HINSTANCE HInstance() 					{ return hInstance; }				// returns owning module handle
-	
+
 
 };
 
@@ -76,7 +85,7 @@ renderHistogram::renderHistogram()
 	: hPanel(nullptr)
 	, iu(nullptr)
 {
-	
+
 }
 
 renderHistogram::~renderHistogram()
@@ -87,6 +96,7 @@ renderHistogram::~renderHistogram()
 void renderHistogram::BeginEditParams(Interface* ip,IUtil* iu) 
 {
 	this->iu = iu;
+	this->ip = ip;
 	hPanel = ip->AddRollupPage(
 		hInstance,
 		MAKEINTRESOURCE(IDD_PANEL),
@@ -94,7 +104,7 @@ void renderHistogram::BeginEditParams(Interface* ip,IUtil* iu)
 		GetString(IDS_PARAMS),
 		0);
 }
-	
+
 void renderHistogram::EndEditParams(Interface* ip,IUtil*)
 {
 	this->iu = nullptr;
@@ -102,9 +112,9 @@ void renderHistogram::EndEditParams(Interface* ip,IUtil*)
 	hPanel = nullptr;
 }
 
-void renderHistogram::Init(HWND /*handle*/)
+void renderHistogram::Init(HWND hWnd/*handle*/)
 {
-
+	CheckDlgButton(hWnd,IDC_R_ACTIVETIME,TRUE);
 }
 
 void renderHistogram::Destroy(HWND /*handle*/)
@@ -112,30 +122,117 @@ void renderHistogram::Destroy(HWND /*handle*/)
 
 }
 
+bool renderHistogram::CheckWindowsMessages(HWND hWnd)
+{
+	MaxSDK::WindowsMessageFilter messageFilter;
+  messageFilter.AddUnfilteredWindow(hWnd);
+  messageFilter.RunNonBlockingMessageLoop();
+  return !messageFilter.Aborted();
+}
+
+void renderHistogram::RenderFrames()
+{
+	//open renderer
+	//remember current parameters
+	//set draft quality parameters
+	//hook the postframe callback
+	//hook the afterrender callback (which removes all the hooks)
+	//start rendering
+	//Interface *ip = GetCOREInterface();
+	ViewParams vp;
+	INode *cam = ip->GetViewExp(NULL).GetViewCamera();
+	
+	Bitmap *bm = NULL;
+	if (!bm) {
+		BitmapInfo bi;
+		bi.SetWidth(320);
+		bi.SetHeight(240);
+		bi.SetType(BMM_FLOAT_RGBA_32);
+		bi.SetFlags(MAP_HAS_ALPHA);
+		bi.SetAspect(1.0f);
+		bm = TheManager->Create(&bi);
+		}
+   bm->Display(_T("AutoExposure"), BMM_RND);
+
+	Interval frames = ip->GetAnimRange();
+	TimeValue startFrame = frames.Start();
+	TimeValue endFrame = frames.End();
+	int delta = GetTicksPerFrame();
+	int res = ip->OpenCurRenderer(cam, NULL, RENDTYPE_NORMAL); //, 320, 240); 
+	for (int frame = startFrame; frame <= endFrame; frame += delta)
+	{
+		ip->CurRendererRenderFrame(frame, bm);
+		if (!CheckWindowsMessages(ip->GetMAXHWnd()))
+			break;
+	}
+	ip->CloseCurRenderer();
+	bm->DeleteThis();
+}
+
 INT_PTR CALLBACK renderHistogram::DlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg) 
 	{
-		case WM_INITDIALOG:
-			renderHistogram::GetInstance()->Init(hWnd);
-			break;
+	case WM_INITDIALOG:
+		renderHistogram::GetInstance()->Init(hWnd);
+		GetInstance()->fromFrame = GetISpinner(GetDlgItem(hWnd, IDC_SPIN_FROM));
+		GetInstance()->fromFrame->SetScale(1.0f);
+		GetInstance()->fromFrame->SetLimits(-1000000, 1000000);
+		GetInstance()->fromFrame->LinkToEdit(GetDlgItem(hWnd, IDC_EDIT_FROM), EDITTYPE_INT);
+		GetInstance()->fromFrame->SetResetValue(0);
 
-		case WM_DESTROY:
-			renderHistogram::GetInstance()->Destroy(hWnd);
-			break;
+		GetInstance()->toFrame = GetISpinner(GetDlgItem(hWnd, IDC_SPIN_TO));
+		GetInstance()->toFrame->SetScale(1.0f);
+		GetInstance()->toFrame->SetLimits(-1000000, 1000000);
+		GetInstance()->toFrame->LinkToEdit(GetDlgItem(hWnd, IDC_EDIT_TO), EDITTYPE_INT);
+		GetInstance()->toFrame->SetResetValue(0);
 
-		case WM_COMMAND:
-			#pragma message(TODO("React to the user interface commands.  A utility plug-in is controlled by the user from here."))
-			break;
+		GetInstance()->ratioVal = GetISpinner(GetDlgItem(hWnd, IDC_SPIN_RATIO));
+		GetInstance()->ratioVal->SetScale(1.0f);
+		GetInstance()->ratioVal->SetLimits(-1000.0f, 1000.0f);
+		GetInstance()->ratioVal->LinkToEdit(GetDlgItem(hWnd, IDC_EDIT_RATIO), EDITTYPE_FLOAT);
+		GetInstance()->ratioVal->SetResetValue(1.0f);
+		GetInstance()->ratioVal->SetValue(1.0f, FALSE);
+		break;
 
-		case WM_LBUTTONDOWN:
-		case WM_LBUTTONUP:
-		case WM_MOUSEMOVE:
-			GetCOREInterface()->RollupMouseMessage(hWnd,msg,wParam,lParam);
-			break;
+	case WM_DESTROY:
+		renderHistogram::GetInstance()->Destroy(hWnd);
+		break;
 
-		default:
-			return 0;
+	case WM_COMMAND:
+#pragma message(TODO("React to the user interface commands."))
+		switch (LOWORD(wParam)) {
+
+		case IDC_DORENDER:
+			GetInstance()->RenderFrames();
+			break;
+		case IDC_APPLYCAMERA:
+			//check if camera is selected
+			//check if average values are calculated
+			//create modifier
+			//create animated paramater
+			//apply to camera
+			break;
+		case IDC_CLOSE:
+			EndDialog(hWnd, FALSE);
+			ReleaseISpinner(GetInstance()->fromFrame);
+			ReleaseISpinner(GetInstance()->toFrame);
+			ReleaseISpinner(GetInstance()->ratioVal);
+			if (GetInstance()->iu)
+				GetInstance()->iu->CloseUtility();
+			break;
+		}
+		break;
+
+	case WM_LBUTTONDOWN:
+
+	case WM_LBUTTONUP:
+	case WM_MOUSEMOVE:
+		GetCOREInterface()->RollupMouseMessage(hWnd,msg,wParam,lParam);
+		break;
+
+	default:
+		return 0;
 	}
 	return 1;
 }
--- resource.h
+++ resource.h
@@ -1,21 +1,32 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by maxProject1.rc
 //
-
 #define IDS_LIBDESCRIPTION              1
 #define IDS_CATEGORY                    2
 #define IDS_CLASS_NAME                  3
 #define IDS_PARAMS                      4
 #define IDS_SPIN                        5
-
 #define IDD_PANEL                       101
-
 #define IDC_CLOSEBUTTON                 1000
 #define IDC_DOSTUFF                     1000
+#define IDC_CLOSE                       1001
+#define IDC_R_ACTIVETIME                1002
+#define IDC_R_RANGE                     1003
+#define IDC_C_SMOOTH                    1004
+#define IDC_DORENDER                    1005
+#define IDC_APPLYCAMERA                 1006
+#define IDC_STATUS                      1007
 #define IDC_COLOR                       1456
 #define IDC_EDIT                        1490
+#define IDC_EDIT_FROM                   1490
+#define IDC_EDIT_TO                     1491
+#define IDC_EDIT_RATIO                  1492
 #define IDC_SPIN                        1496
+#define IDC_SPIN_FROM                   1496
+#define IDC_SPIN_TO                     1497
+#define IDC_SPIN3                       1498
+#define IDC_SPIN_RATIO                  1498
 
 // Next default values for new objects
 // 
@@ -23,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
